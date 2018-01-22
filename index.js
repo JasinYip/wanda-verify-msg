b1c6d14487ab13a0d9c6c719be313e4ac8d9648f
叶俊星 <yejunxing@meituan.com>
2018-01-22 12:35:55 +0800

    Feat: Add copy-to-clipboard feature.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
+const path = require('path')
 const app = require('express')()
 
 app.listen('2048')
 
 app.get('/', (req, res) => {
-  res.set({ 'content-type': 'application/json; charset=utf-8' })
-
-  const msg = `欢迎你的申请！请按以下步骤验证你的余额：
-1、向你的一个地址转 1000${(''+Math.random()).substr(0, 8)} 枚 ADA；
-2、转账完成后，回复交易 ID。` 
-
-  res.end(msg)
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
